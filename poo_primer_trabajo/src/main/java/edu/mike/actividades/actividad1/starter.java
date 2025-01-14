8f8d29fbb4f01bc2b44681f1bcd33a90d7cba731
mikeess05 <miguelmendoza.05.mm@gmail.com>
2025-01-14 17:16:15 -0600

    Poo

---
+++ poo_primer_trabajo/src/main/java/edu/mike/actividades/actividad1/starter.java
@@ -0,0 +1,13 @@
+package edu.mike.actividades.actividad1;
+
+import edu.mike.actividades.actividad1.ui.CLI;
+
+public class starter {
+
+    public static void main(String[] args) {
+        CLI.ShowMenu();
+    }
+
+
+
+}
---
+++ poo_primer_trabajo/src/main/java/edu/mike/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,12 @@
+package edu.mike.actividades.actividad1.ui;
+
+public class CLI {
+
+    public static void ShowMenu(){
+
+        System.out.println("Bienvenido a la aplicacion 1");
+        System.out.println("1.Crear carro");
+        System.out.println("2.Mostrar carro");     
+    }
+
+}
