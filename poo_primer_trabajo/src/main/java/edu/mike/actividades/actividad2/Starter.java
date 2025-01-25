c6d878a34e7824aa042e32da2f170d71436ba83f
mikeess05 <miguelmendoza.05.mm@gmail.com>
2025-01-24 21:34:41 -0600

    Problema de leetcode semanal

--- poo_primer_trabajo/src/main/java/edu/mike/actividades/actividad2/Starter.java
+++ poo_primer_trabajo/src/main/java/edu/mike/actividades/actividad2/Starter.java
@@ -1,7 +1,9 @@
 package edu.mike.actividades.actividad2;
 import edu.mike.actividades.actividad2.ui.CLI;
 
-
+/**
+ * Clase que ejecuta la aplicacion
+ */
 public class Starter {
     public static void main(String[] args) {
         CLI.runApp();
--- poo_primer_trabajo/src/main/java/edu/mike/actividades/leetcode/Problema1Semanal.java
+++ poo_primer_trabajo/src/main/java/edu/mike/actividades/leetcode/Problema1Semanal.java
@@ -1,9 +1,33 @@
 package edu.mike.actividades.leetcode;
 
+import java.util.Arrays;
+
 public class Problema1Semanal {
 
     public static void main(String[] args) {
+        int[] nums = {-7, -2, 2, 15};
         
+        int[] resultado = new int[nums.length];
+        int izquierda = 0;
+        int derecha = nums.length - 1;
+        int indice = derecha;
 
+        while (izquierda<=derecha) {
+            int cuadradoIzquierda = nums[izquierda] * nums[izquierda];
+            int cuadradoDerecha = nums[derecha] * nums[derecha];
+            
+            if (cuadradoIzquierda > cuadradoDerecha) {
+                resultado[indice] = cuadradoIzquierda;
+                izquierda++;
+            } else {
+                resultado[indice] = cuadradoDerecha;
+                derecha--;
+            }
+            indice--;
+        }
+        System.out.println(Arrays.toString(resultado));
+        
     }
+
 }
+
